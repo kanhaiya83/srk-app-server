4b90820d65299a5bd90f1388e2fdcd8987b51596
Kanhaiya <kanhaiya2778@gmail.com>
2025-03-18 18:42:23 +0530

    fixes

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,12 +1,12 @@
 import admin, { ServiceAccount }  from 'firebase-admin'
 import {config}  from 'dotenv'
 config()
-
+const key = JSON.parse(process.env.FIREBASE_PRIVATE_KEY!)["key"]
 const serviceAccount = {
     "type": "service_account",
     "project_id": "srk-auction-86a90",
     "private_key_id": "f6eec3ed97ba5f76703c44f9444d73eac6b150c8",
-    "private_key": process.env.FIREBASE_PRIVATE_KEY,
+    "private_key": key,
     "client_email": "firebase-adminsdk-fbsvc@srk-auction-86a90.iam.gserviceaccount.com",
     "client_id": "109663606567632511689",
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
